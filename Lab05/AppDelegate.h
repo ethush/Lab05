2c1e55e6d873bbdca3d4619357c97623813a8ea7
alebautt <id_bautista@hotmail.com>
2015-01-24 10:29:04 -0600

    Initial Commit

---
+++ Lab05/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab05
+//
+//  Created by Alejandra B on 24/01/15.
+//  Copyright (c) 2015 alebautista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lab05/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Lab05
+//
+//  Created by Alejandra B on 24/01/15.
+//  Copyright (c) 2015 alebautista. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
